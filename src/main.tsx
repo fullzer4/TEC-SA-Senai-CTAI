6ad9bcb9dee40d7d9f4354c52f5f61cfe857a6fd
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-06-15 23:30:48 -0300

    att

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import Rota from './routes/route'
+import "./index.css"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Rota />
   </React.StrictMode>
 )
