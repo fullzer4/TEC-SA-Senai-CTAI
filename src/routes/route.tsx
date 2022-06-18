fa24abc45af1f370330584d0fcc74442c9fad252
Bruno Ricardo Wotzke <brunoricardowotzke@gmail.com>
2022-06-18 16:27:53 -0300

    sa

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom"
 import Home from "../pages/Home"
 import Calculadora from "../pages/Calculadora"
+import Linguagens from "../pages/Linguagens"
+import Projeto from
 
 export default function Rota() {
   return(
@@ -8,6 +10,9 @@ export default function Rota() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/calculator" element={<Calculadora/>}/>
+        <Route path="/projects" element={<Linguagens/>}/>
+        <Route path="/about" element={<Projeto/>}/>
+        <Route path="/languages" element={<Sobre/>}/>
       </Routes>
     </BrowserRouter>
   )
