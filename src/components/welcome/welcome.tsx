7140dc2aa5558b1adb62c663b6cc88198cabb708
Bruno Ricardo Wotzke <brunoricardowotzke@gmail.com>
2022-06-18 20:13:54 -0300

    sa

--- src/components/welcome/welcome.tsx
+++ src/components/welcome/welcome.tsx
@@ -1,4 +1,5 @@
-import logo from "../../images/logo.png";
+import logo from "../../images/logo2.png";
+import back from "../../images/back1.png";
 import "./welcome.css"
 
 export default function Welcome(){
@@ -8,13 +9,14 @@ export default function Welcome(){
             <div className="welcome-in">
               <h1 className="title-welcome">Bem vindo(a)</h1>
               <p className="text-welcome">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad iusto eligendi inventore, architecto dicta ut error impedit.</p>
-              <button className="button-welcome">Welcome</button>
+              <button className="button-welcome">Comece a usar</button>
             </div>
             <div className="welcome-image">
               <img className="img-ini" src={logo} alt=""/>
             </div>
           </div>
           <div className="triangle-down"></div>
+          <img className="back-img" src={back} alt=""/>
         </div>
     )
 }
