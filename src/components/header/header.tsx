75619a62a6fc49a38530fff15b0e2c75b0c8d0f2
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-06-19 15:28:23 -0300

    att

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,19 +4,23 @@ import { Link } from "react-router-dom";
 import AOS from "aos"
 
 export default function Header(){
+    AOS.init({
+        delay: 100,
+        duration: 500
+    })
     return(
         <header>
             <div className="def-h left">
-                <Link to="../" className="bt-def-h logo">
-                    <img className="logo-img" src={logo} alt="aaaaa"/>
+                <Link to="../" className="bt-def-h logo" data-aos="fade-down">
+                    <img className="logo-img" src={logo} alt="logo"/>
                     TEC SA
                 </Link>
             </div>
             <div className="def-h right">
-                <Link to="/projects" className="bt-def-h">Projeto</Link>
-                <Link to="/about" className="bt-def-h">Sobre</Link>
-                <Link to="/calculator" className="bt-def-h">Calculadora</Link>
-                <Link to="/languages" className="bt-def-h">Linguagens</Link>
+                <Link to="/projects" className="bt-def-h" data-aos="fade-down">Projeto</Link>
+                <Link to="/about" className="bt-def-h" data-aos="fade-down">Sobre</Link>
+                <Link to="/calculator" className="bt-def-h" data-aos="fade-down">Calculadora</Link>
+                <Link to="/languages" className="bt-def-h" data-aos="fade-down">Linguagens</Link>
             </div>
         </header>
     )
--- src/components/welcome/welcome.tsx
+++ src/components/welcome/welcome.tsx
@@ -2,24 +2,28 @@ import back from "../../images/back1.png";
 import Logo from "../logo/logo";
 import "../logo/logo.css"
 import "./welcome.css"
-
+import AOS from "aos"
 
 
 export default function Welcome(){
-    return(
-        <div className="cont-out">
-          <div className="welcome-out">
-            <div className="welcome-in">
-              <h1 className="title-welcome">Bem vindo(a)</h1>
-              <p className="text-welcome">Calculadora para trabalho SA entre materias diciplinares, técnico e natureza.</p>
-              <button className="button-welcome">Comece a usar</button>
-            </div>
-            <div className="welcome-image">
-              <Logo/>
-            </div>
-          </div>
-          <div className="triangle-down"></div>
-          <img className="back-img" src={back} alt=""/>
+  AOS.init({
+    delay: 100,
+    duration: 550
+  })
+  return(
+    <div className="cont-out">
+      <div className="welcome-out">
+        <div className="welcome-in" data-aos="fade-up">
+          <h1 className="title-welcome">Bem vindo(a)</h1>
+          <p className="text-welcome">Calculadora para trabalho SA entre materias diciplinares, técnico e natureza.</p>
+          <button className="button-welcome">Comece a usar</button>
         </div>
-    )
+          <div className="welcome-image">
+            <Logo/>
+          </div>
+        </div>
+      <div className="triangle-down"></div>
+      <img className="back-img" src={back} alt=""/>
+    </div>
+  )
 }
