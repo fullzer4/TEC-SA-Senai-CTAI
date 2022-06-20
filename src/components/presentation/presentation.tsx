53a838c446de6c2429c7b11517b3ff1dc8312f8f
Bruno Ricardo Wotzke <brunoricardowotzke@gmail.com>
2022-06-19 23:01:29 -0300

    sa

--- src/components/presentation/presentation.tsx
+++ src/components/presentation/presentation.tsx
@@ -11,29 +11,6 @@ export default function WelcomeBottom(){
                 <Nat/>
                 <Tec/>
             </div>
-
-            {/* ignorar daqui p baixo - area de teste */}
-
-            <h1 className="title">Nosso grupo</h1>
-            <p className="text" >Nosso grupo é formado por 5 participantes, estamos desenvolvendo um projeto para uma SA entre as seguintes áreas diciplinares: Técnico e Natureza.</p>
-            <div className="svg-icon-out">
-                <Nat/>
-                <Tec/>
-            </div>
-
-            <h1 className="title">Nosso grupo</h1>
-            <p className="text" >Nosso grupo é formado por 5 participantes, estamos desenvolvendo um projeto para uma SA entre as seguintes áreas diciplinares: Técnico e Natureza.</p>
-            <div className="svg-icon-out">
-                <Nat/>
-                <Tec/>
-            </div>
-            
-            <h1 className="title">Nosso grupo</h1>
-            <p className="text" >Nosso grupo é formado por 5 participantes, estamos desenvolvendo um projeto para uma SA entre as seguintes áreas diciplinares: Técnico e Natureza.</p>
-            <div className="svg-icon-out">
-                <Nat/>
-                <Tec/>
-            </div>
         </div>
     )
 }
