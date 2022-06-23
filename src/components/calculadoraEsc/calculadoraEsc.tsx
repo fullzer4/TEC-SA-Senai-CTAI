d1abe210e24198b19e988a0ecde98b4006351b48
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-06-23 09:31:21 -0300

    att

--- src/components/calculadoraEsc/calculadoraEsc.tsx
+++ src/components/calculadoraEsc/calculadoraEsc.tsx
@@ -1,20 +1,23 @@
+import { useContext } from "react"
+import { CalculosContext } from "../../contexts/calculos"
 import "./calculadoraEsc.css"
 
 export default function CalculadoraEsc() {
+  const { classSC, verClickCalc} = useContext(CalculosContext)
 
     return(
       <div className="calculadora">
-        <div className="show-calculo">
+        <div className={classSC}>
           <div className="calculo1">
             <h4>foi</h4>
-            <button>sair</button>
+            <button onClick={verClickCalc}>sair</button>
           </div>
         </div>
         <div className="calculadora-boxes">
           <div className="calculadora-box">
             <h2>Nome Calc</h2>
             <h3>explicao do calculo e bla bla bla</h3>
-            <button>Abrir</button>
+            <button onClick={verClickCalc}>Abrir</button>
           </div>
         </div>
       </div>
--- src/contexts/calculos.tsx
+++ src/contexts/calculos.tsx
@@ -7,22 +7,45 @@ type CalculosContextProps = { //configurar as props
 type CalculosContextType ={ //tipo
     isOpenModal: boolean;
     setIsOpenModal: (newState: boolean) => void;
+    classSC: string;
+    verClickCalc: () => void;
+    openCalc: boolean;
 }
 
 const CalculosinitialValue ={  //definir o que ele ira receber
     isOpenModal: false,
     setIsOpenModal: () => {},
+    classSC: ("show-calculo desativado"),
+    verClickCalc: () => {},
+    openCalc: false,
 }
 
 export const CalculosContext = createContext<CalculosContextType>(CalculosinitialValue)
 
 export const CalculosProvider = ({ children }: CalculosContextProps) => {
     const [isOpenModal, setIsOpenModal] = useState(CalculosinitialValue.isOpenModal)
+    const [classSC, setClassSC] = useState(CalculosinitialValue.classSC)
+    const [openCalc, setOpenCalc] = useState(CalculosinitialValue.openCalc)
+
+    function verClickCalc(){
+        if(openCalc === false){
+            setOpenCalc(true)
+            setClassSC("show-calculo")
+        }if(openCalc === true){
+            setOpenCalc(false)
+            setClassSC("show-calculo desativado")
+        }else{
+            console.log("erro backend");
+        }
+    }
 
     return(
         <CalculosContext.Provider value={{
             isOpenModal,
             setIsOpenModal,
+            classSC,
+            verClickCalc,
+            openCalc
             }}> 
             {children}
         </CalculosContext.Provider>
