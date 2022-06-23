c000c16d0d4e4fba34c2b2a1e6095953cee5e5d3
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-06-23 11:05:49 -0300

    att

--- src/components/calculointro/calculoIntro.tsx
+++ src/components/calculointro/calculoIntro.tsx
@@ -1,10 +1,12 @@
 import "./calculoIntro.css"
+import back from "../../images/back1.png"
 export default function CalculoIntro() {
 
   return(
     <div className="calculo-intro">
       <h1 className="calculo-intro-texto">Calculadora</h1>
       <div className="triangle-down"></div>
+      <img className="back-img" src={back} alt=""/>
     </div>
   )
 }
