317f9fd6038d6a37375912e2cea528d5162c3f02
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-06-28 00:52:34 -0300

    att

--- src/components/calculadoraEsc/calculadoraEsc.tsx
+++ src/components/calculadoraEsc/calculadoraEsc.tsx
@@ -91,19 +91,19 @@ export default function CalculadoraEsc() {
 
           <div className="calculadora-box c-3d">
             <h2 className="title-calc">Energia Interna Temperatura</h2>
-            <h3 className="text-calc">3 / 2 nRT = U <p/> Calculos Possiveis:<p/> 3 / 2 nRT = x<p/> 3 / 2 nRx = U<p/> 3 / 2 nxT = U<p/>  3 / 2 xRT = U<p/>n = Numero de Mols do Gás <p/> R = Constante Universal dos Gases Perfeitos <p/> T = Temperatura Absoluta <p/>Todas as medidas dessa calculadora estão em jaule(J) <p/></h3>
+            <h3 className="text-calc">3 / 2 nRT = U <p/> Calculos Possiveis:<p/> 3 / 2 nRT = x<p/> 3 / 2 nRx = U<p/>  3 / 2 xRT = U<p/>n = Numero de Mols do Gás <p/> R = Constante Universal dos Gases Perfeitos <p/> T = Temperatura Absoluta <p/>Todas as medidas dessa calculadora estão em jaule(J) <p/></h3>
             <button className="button-calc" onClick={verClickCalc5}>Abrir</button>
           </div>
 
           <div className="calculadora-box c-3d">
             <h2 className="title-calc">Variação de Energia Interna</h2>
-            <h3 className="text-calc">3 / 2 nRΔT = ΔU <p/> Calculos Possiveis:<p/> 3 / 2 nRΔT = x<p/> 3 / 2 nRx = ΔU<p/> 3 / 2 nxΔT = ΔU<p/> 3 / 2 xRΔT = ΔU<p/>n = Numero de Mols do Gás<p/> R = Constante Universal dos Gases Perfeitos <p/> ΔT = Variação de Temperatura Absoluta <p/> ΔU = Variação de Energia Interna<p/>Todas as medidas dessa calculadora estão em jaule(J) <p/></h3>
+            <h3 className="text-calc">3 / 2 nRΔT = ΔU <p/> Calculos Possiveis:<p/> 3 / 2 nRΔT = x<p/> 3 / 2 nRx = ΔU<p/> 3 / 2 xRΔT = ΔU<p/>n = Numero de Mols do Gás<p/> R = Constante Universal dos Gases Perfeitos <p/> ΔT = Variação de Temperatura Absoluta <p/> ΔU = Variação de Energia Interna<p/>Todas as medidas dessa calculadora estão em jaule(J) <p/></h3>
             <button className="button-calc" onClick={verClickCalc5}>Abrir</button>
           </div>
       
           <div className="calculadora-box c-3d">
             <h2 className="title-calc">Trabalho de um Gás Sob Temperatura Constante</h2>
-            <h3 className="text-calc">nRTln(V2 / V1) = T <p/> Calculos Possiveis:<p/> nRTln(V2 / V1) = x<p/> nRTn(V2 / x) = T<p/> nRTln(x / V1) = T<p/> nRTx(V2 / V1) = T<p/>nxln(V2 / V1) = T<p/>xRTln(V2 / V1) = T<p/>n = Numero de Mols do Gás <p/> R = Constante Universal dos Gases Perfeitos <p/> T = Temperatura Absoluta <p/> V1 = Volume na Etapa Inicial <p/> V2 = Volume na Etapa Final <p/>Todas as medidas dessa calculadora estão em jaule(J) <p/></h3>
+            <h3 className="text-calc">nRTln(V2 / V1) = T <p/> Calculos Possiveis:<p/> nRTln(V2 / V1) = x<p/> nRTn(V2 / x) = T<p/> nRTln(x / V1) = T<p/> nRTx(V2 / V1) = T<p/>xRTln(V2 / V1) = T<p/>n = Numero de Mols do Gás <p/> R = Constante Universal dos Gases Perfeitos <p/> T = Temperatura Absoluta <p/> V1 = Volume na Etapa Inicial <p/> V2 = Volume na Etapa Final <p/>Todas as medidas dessa calculadora estão em jaule(J) <p/></h3>
             <button className="button-calc" onClick={verClickCalc5}>Abrir</button>
           </div>
 
