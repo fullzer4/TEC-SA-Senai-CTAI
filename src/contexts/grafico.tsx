93f78939c6645185481a01c43f040d5314d5b7e7
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-06-29 20:25:34 -0300

    att

---
+++ src/contexts/grafico.tsx
@@ -0,0 +1,31 @@
+import { createContext, ReactNode, useState } from "react";
+import firebase from "../services/firebaseConnection"
+
+type GraficoContextProps = { //configurar as props
+    children: ReactNode;
+}
+
+type GraficoContextType ={ //tipo
+    teste: boolean;
+    setTeste: (newState: boolean) => void;
+}
+
+const GraficoinitialValue ={  //definir o que ele ira receber
+    teste: false,
+    setTeste: () => {},
+}
+
+export const GraficoContext = createContext<GraficoContextType>(GraficoinitialValue)
+
+export const GraficoProvider = ({ children }: GraficoContextProps) => {
+    const [teste, setTeste] = useState(GraficoinitialValue.teste)
+
+    return(
+        <GraficoContext.Provider value={{
+            teste,
+            setTeste
+            }}> 
+            {children}
+        </GraficoContext.Provider>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,15 @@ import Rota from './routes/route'
 import "./index.css"
 import { CalculosProvider } from './contexts/calculos'
 import { ToastContainer } from "react-toastify"
+import { GraficoProvider } from './contexts/grafico';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <CalculosProvider>
+    <GraficoProvider>
       <ToastContainer autoClose={2000}/>
       <Rota />
+    </GraficoProvider>
     </CalculosProvider>
   </React.StrictMode>
 )
