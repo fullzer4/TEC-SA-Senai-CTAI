bd77f5ada4a556ba5dc47f0ca692eb389cb3f231
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-06-30 18:59:28 -0300

    att

--- src/contexts/grafico.tsx
+++ src/contexts/grafico.tsx
@@ -7,16 +7,17 @@ type GraficoContextProps = { //configurar as props
 
 type GraficoContextType ={ //tipo
     getDadosFB: () => void;
+    envDadosFB: (newState: number) => void;
 }
 
 const GraficoinitialValue ={  //definir o que ele ira receber
     getDadosFB: () => {},
+    envDadosFB: () => {},
 }
 
 export const GraficoContext = createContext<GraficoContextType>(GraficoinitialValue)
 
 export const GraficoProvider = ({ children }: GraficoContextProps) => {
-    const CalculosAll = []
 
     async function getDadosFB(){
         await firebase.firestore().collection("Calculos").onSnapshot((doc)=>{
@@ -28,15 +29,73 @@ export const GraficoProvider = ({ children }: GraficoContextProps) => {
 
     async function envDadosFB(calculo:number) {
         if(calculo === 1){
-            await firebase.firestore().collection("Calculos").doc("Lei1").onSnapshot({
+            await firebase.firestore().collection("Calculos").doc("Registro").collection("1 Lei")
+            .add({
 
             })
+        }
+        if(calculo === 2){
+            await firebase.firestore().collection("Calculos").doc("Registro").collection("2 Lei")
+            .add({
+
+            })
+        }
+        if(calculo === 3){
+            await firebase.firestore().collection("Calculos").doc("Registro").collection("Efic energ")
+            .add({
+
+            })
+        }
+        if(calculo === 4){
+            await firebase.firestore().collection("Calculos").doc("Registro").collection("Rend Maq")
+            .add({
+
+            })
+        }
+        if(calculo === 5){
+            await firebase.firestore().collection("Calculos").doc("Registro").collection("Trab Maq")
+            .add({
+
+            })
+        }
+        if(calculo === 6){
+            await firebase.firestore().collection("Calculos").doc("Registro").collection("Energ Int V")
+            .add({
+
+            })
+        }
+        if(calculo === 7){
+            await firebase.firestore().collection("Calculos").doc("Registro").collection("Trab Gas P")
+            .add({
+
+            })
+        }
+        if(calculo === 8){
+            await firebase.firestore().collection("Calculos").doc("Registro").collection("Energ Int T")
+            .add({
+
+            })
+        }
+        if(calculo === 9){
+            await firebase.firestore().collection("Calculos").doc("Registro").collection("Varia Energ")
+            .add({
+
+            })
+        }
+        if(calculo === 10){
+            await firebase.firestore().collection("Calculos").doc("Registro").collection("Trab Gas T")
+            .add({
+
+            })
+        }else{
+            return
         }
     }
 
     return(
         <GraficoContext.Provider value={{
-            getDadosFB
+            getDadosFB,
+            envDadosFB
             }}> 
             {children}
         </GraficoContext.Provider>
--- src/pages/test/test.tsx
+++ src/pages/test/test.tsx
@@ -2,12 +2,13 @@ import React, {Component, useContext} from 'react';
 import { GraficoContext } from '../../contexts/grafico';
 
   
-    export default function Buceta(){
-    const {getDadosFB} = useContext(GraficoContext)
+export default function Buceta(){
     
-        return (
-            <div>
-                <button onClick={getDadosFB}>AAAAAAAA</button>
-            </div>
-        );
-    }
+    const { envDadosFB } = useContext(GraficoContext)
+
+    return (
+        <div>
+            <button onClick={() => envDadosFB(1)}>AAAAAAAA</button>
+        </div>
+    );
+}
