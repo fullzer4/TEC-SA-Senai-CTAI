5cc5229b923f4c6440401f91212f251b1336e9c1
fullzer4 <53914594+fullzer4@users.noreply.github.com>
2022-07-01 08:11:49 -0300

    att

--- src/pages/test/test.tsx
+++ src/pages/test/test.tsx
@@ -1,16 +1,22 @@
 import {Component, useContext} from 'react';
 import { GraficoContext } from '../../contexts/grafico';
 
+
   
 export default function Buceta(){
     
-    const { envDadosFB, getDadosFB } = useContext(GraficoContext)
+    const { envDadosFB, getDadosFB, size1 } = useContext(GraficoContext) 
+
+    setTimeout(()=>{
+        envDadosFB(1);
+        getDadosFB(1);    
+    },1000)
 
     return (
         <div>
-            <h1></h1>
+            <h1>{size1}</h1>
             <button onClick={() => getDadosFB(1)}>Receber</button>
             <button onClick={() => envDadosFB(1)}>Enviar</button>
-        </div>
+        </div> 
     );
 }
