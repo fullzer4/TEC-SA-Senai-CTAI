42d7ec227443c584c071dd4b32c98702a82486a4
fullzer4 <53914594+fullzer4@users.noreply.github.com>
2022-07-04 15:12:33 -0300

    att

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -1,12 +1,53 @@
+import { Link } from 'react-router-dom';
 import Logo from '../../images/logo/logo'
+import Calc from '../../images/calc/calc'
+import Team from '../../images/team/team'
+import Teoric from '../../images/teoric/teoric'
+import Scientist from '../../images/scientist/scientist'
+import Graphic from '../../images/graphic/graphic'
+
 import './navbar.css';
 export default function Navbar() {
     return(
         <div id="navbar">
-            <button className="btn-nav">
-                <Logo/>
-            </button>
+            <Link to="/">
+                <button className="btn-nav">
+                    <Logo/>
+                </button>
+            </Link>
+
+
             <hr/>
+
+            <Link to="/test">
+                <button className="btn-nav">
+                    <Calc/>
+                </button>
+            </Link>
+
+            <Link to="/">
+                <button className="btn-nav">
+                    <Team/>
+                </button>
+            </Link>
+
+            <Link to="/">
+                <button className="btn-nav">
+                    <Teoric/>
+                </button>
+            </Link>
+
+            <Link to="/">
+                <button className="btn-nav">
+                    <Scientist/>
+                </button>
+            </Link>
+
+            <Link to="/">
+                <button className="btn-nav">
+                    <Graphic/>
+                </button>
+            </Link>
         </div>
     )
   }
---
+++ src/images/teoric/teoric.tsx
@@ -0,0 +1,12 @@
+import './teoric.css';
+export default function Logo(){
+    return(
+        <svg width="35" height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1.34613 11.6667L16.1538 19.8333L33.6538 9.33334L18.8461 1.16667L1.34613 11.6667Z" stroke="#E7E7E7" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M1.34613 11.6667V18.6667L16.1538 26.8333L33.6538 16.3333C32.0384 14.9333 31.6346 12.8333 32.7115 11.0833L33.6538 9.33333" stroke="#E7E7E7" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M1.34613 18.6667V25.6667L16.1538 33.8333L33.6538 23.3333C32.0384 21.9333 31.6346 19.8333 32.7115 18.0833L33.6538 16.3333" stroke="#E7E7E7" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+    )
+}
+
+
