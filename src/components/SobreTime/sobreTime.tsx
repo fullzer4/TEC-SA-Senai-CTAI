ccfe2c8a72c33afc24e55af7db174b03e426219d
fullzer4 <53914594+fullzer4@users.noreply.github.com>
2022-07-05 11:48:22 -0300

    att

--- src/components/SobreTime/sobreTime.tsx
+++ src/components/SobreTime/sobreTime.tsx
@@ -1,14 +1,14 @@
 import Equipe2 from "../../images/HomeEquipe2.svg"
+import "./sobreTime.css"
+
 export default function SobreTime() {
     return(
         <div id="sobretime">
-            <div className="">
-                <h3>Sobre nosso time</h3>
-                <div>
-                    <img src={Equipe2}></img>
-                </div>
-                <div>
-                    <h4>Nosso grupo é formado por 5 participantes, estamos desenvolvendo um projeto para uma SA entre as seguintes áreas diciplinares: Técnico e Natureza. Saiba mais sobre nós.</h4>
+            <div className="sobretime-box">
+                <h3 className="sobretime-box-titulo">Sobre nosso time</h3>
+                <div className="sobretime-box-conteudo">
+                    <img src={Equipe2} className="sobretime-box-conteudo-imagem"></img>
+                    <h4 className="sobretime-box-conteudo-texto">Nosso grupo é formado por 5 participantes, estamos desenvolvendo um projeto para uma SA entre as seguintes áreas diciplinares: Técnico e Natureza. Saiba mais sobre nós.</h4>
                 </div>
             </div>
         </div>
