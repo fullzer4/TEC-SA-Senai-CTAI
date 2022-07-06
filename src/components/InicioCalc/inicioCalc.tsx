79924d8e29762b79f16f0e30f01eeb1b90bd7bba
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-07-05 22:44:10 -0300

    att

--- src/components/InicioCalc/inicioCalc.tsx
+++ src/components/InicioCalc/inicioCalc.tsx
@@ -1,15 +1,17 @@
 import { Link } from "react-router-dom";
+import Equipe1 from "../../images/CalcEquipe1.svg"
+import "./inicioCalc.css"
 
 export default function InicioCalc() {
     return(
         <div id="iniciocalc">
-            <div>
-                <img></img>
+            <div className="iniciocalc-box-imagem">
+                <img src={Equipe1}></img>
             </div>
-            <div>
-                <h1></h1>
-                <h2></h2>
-                <Link to="/test"></Link>
+            <div className="iniciocalc-box-texto">
+                <h1 className="iniciocalc-box-texto-titulo">Calculadoras</h1>
+                <h2 className="iniciocalc-box-texto-subtitulo">As medidas das calculadoras estão em Jaules e coneguem realizar prova real alem de guardamos os dados para realizar analises</h2>
+                <Link to="/test" className="iniciocalc-box-texto-botao">Ver analises</Link>
             </div>
         </div>
     )
