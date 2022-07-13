a8cfe2e1699008931865ec4ed4a5572972848ca0
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-07-12 22:35:38 -0300

    att

--- src/components/inicioGraphics/inicioGraphics.tsx
+++ src/components/inicioGraphics/inicioGraphics.tsx
@@ -1,14 +1,14 @@
 import { Link } from "react-router-dom";
-import Equipe1 from "../../images/CalcEquipe1.svg"
+import Equipe1 from "../../images/timeGrafico1.svg"
 import "./inicioGraphics.css"
 
 export default function inicioGraphics() {
     return(
         <div className="container-out reverse">
             <div className="out-in">
-                <h1 className="title-ini">Calculadoras</h1>
-                <p className="text-ini">As medidas das calculadoras estão em Jaules e coneguem realizar prova real alem de guardamos os dados para realizar analises </p>
-                <Link to="/test"><button className="bt-link">Ver analises</button></Link>
+                <h1 className="title-ini">Dados do Site</h1>
+                <p className="text-ini">Aqui mostramos todos os dados que coletamos da pagina de calculos.</p>
+                <Link to="/test"><button className="bt-link">Ir para calculos</button></Link>
             </div>
             <div className="out-in">
                 <img src={Equipe1} alt=""/>
