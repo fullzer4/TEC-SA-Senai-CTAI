e1e48baebd0341583d8cdc7fded977eef06955fd
fullzer4 <53914594+fullzer4@users.noreply.github.com>
2022-07-19 09:52:22 -0300

    att

--- src/components/calc/cont.tsx
+++ src/components/calc/cont.tsx
@@ -4,7 +4,7 @@ import "./calc.css"
 
 
 export default function ContCalc() {
-    const { classSC, verClickSair, calcEsc, pc1Esc, pc2Esc, pcResult, val1Esc, setVal1Esc, val2Esc, setVal2Esc, result, limpar} = useContext(CalculosContext)
+    const { classSC, verClickSair, calcEsc, pc1Esc, pc2Esc, pcResult, val1Esc, setVal1Esc, val2Esc, setVal2Esc, result, setResult, limpar, verificarLei} = useContext(CalculosContext)
     return(
         <div className={ classSC }>
             <div className="cont-calc-in">
@@ -12,11 +12,17 @@ export default function ContCalc() {
                 <h1 className="title-calc">{calcEsc}</h1>
                 <div className="func-out-calcs">
                     <div className="inps-out uiashd">
-                        <input className="inp-cont-calc" placeholder={pc1Esc}/>
-                        <input className="inp-cont-calc" placeholder={pc2Esc}/>
-                        <input className="inp-cont-calc" placeholder={pcResult} value={result}/>
+                        <input type="number" className="inp-cont-calc" placeholder={pc1Esc} value={val1Esc} onChange={(e)=> setVal1Esc(e.target.value)}/>
+                        <input type="number" className="inp-cont-calc" placeholder={pc2Esc} value={val2Esc} onChange={(e)=> setVal2Esc(e.target.value)}/>
+                        <input type="number" className="inp-cont-calc" placeholder={pcResult} value={result} onChange={(e)=> setResult(e.target.value)}/>
                     </div>
-                    <div className="hst-out uiashd"></div>
+                    <div className="hst-out uiashd">
+
+                    </div>
+                </div>
+                <div className="calc-botoes">
+                    <button onClick={()=> limpar()}>Limpar</button>
+                    <button onClick={()=> verificarLei()}>Calcular</button>
                 </div>
             </div>
         </div>
