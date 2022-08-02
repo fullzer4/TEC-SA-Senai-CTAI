15fabaae79099cb29662a87cda06978b87ed6807
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-08-02 05:07:56 -0300

    att

--- src/pages/calculadora/Calculadora.tsx
+++ src/pages/calculadora/Calculadora.tsx
@@ -1,13 +1,18 @@
 import InicioCalc from '../../components/InicioCalc/inicioCalc';
 import Calc from '../../components/calc/calc';
 import Header from '../../components/Navbar/navbar';
+import AOS from "aos"
+
 export default function Calculadora() {
+    AOS.init({
+        delay: 150,
+      })
 
     return(
         <div id="container ">
             <div id="content">
                 <Header/>
-                <div id="content-objects">
+                <div id="content-objects" data-aos="fade-in">
                     <InicioCalc/>
                     <Calc/>
                 </div>
--- src/pages/experimento/Experimento.tsx
+++ src/pages/experimento/Experimento.tsx
@@ -1,12 +1,17 @@
 import Experiment from '../../components/experiment/experiment';
 import Header from '../../components/Navbar/navbar';
+import AOS from "aos"
 
 export default function Experimento() {
+    AOS.init({
+        delay: 150,
+      })
+
     return(
         <div id="container">
             <div id="content">
                 <Header/>
-                <div id="content-objects">
+                <div id="content-objects" data-aos="fade-in">
                     <Experiment/>
                 </div>
             </div>
