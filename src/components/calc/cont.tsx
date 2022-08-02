b97718f53d7f9f5cba6ff8759ab4398061f2c894
Gabriel Pelizzaro <53914594+fullzer4@users.noreply.github.com>
2022-08-02 03:56:56 -0300

    att

--- src/components/calc/cont.tsx
+++ src/components/calc/cont.tsx
@@ -5,7 +5,28 @@ import Hist from "../../images/Historic.svg"
 import "./calc.css"
 
 export default function ContCalc() {
-    const { classSC, verClickSair, calcEsc, pc1Esc, pc2Esc, pcResult, val1Esc, setVal1Esc, val2Esc, setVal2Esc, result, setResult, limpar, verificarLei, sendbd} = useContext(CalculosContext)
+    const {envDadosFB, classSC, verClickSair, calcEsc, pc1Esc, pc2Esc, pcResult, val1Esc, setVal1Esc, val2Esc, setVal2Esc, result, setResult, limpar, verificarLei, sendbd} = useContext(CalculosContext)
+    function rodar(number:number){
+        if(number === 1){verificarLei(sendbd),envDadosFB(1)}
+        if(number === 2){verificarLei(sendbd),envDadosFB(2)}
+        if(number === 3){verificarLei(sendbd),envDadosFB(3)}
+        if(number === 4){verificarLei(sendbd),envDadosFB(4)}
+        if(number === 5){verificarLei(sendbd),envDadosFB(5)}
+        if(number === 6){verificarLei(sendbd),envDadosFB(6)}
+        if(number === 7){verificarLei(sendbd),envDadosFB(7)}
+        if(number === 8){verificarLei(sendbd),envDadosFB(8)}
+        if(number === 9){verificarLei(sendbd),envDadosFB(9)}
+    }
+    if(calcEsc === "1 Lei Termodinamica"){const calculoHEHE = 1}
+    if(calcEsc === "2 Lei Termodinamica"){const calculoHEHE = 2}
+    if(calcEsc === "Eficiencia Energetica"){const calculoHEHE = 3}
+    if(calcEsc === "Rendimento Maquina Termica"){const calculoHEHE = 4}
+    if(calcEsc === "Trabalho Maquina Termica"){const calculoHEHE = 5}
+    if(calcEsc === "Energia Interna Volume"){const calculoHEHE = 6}
+    if(calcEsc === "Trabalho de um Gás Sob Pressão Constante"){const calculoHEHE = 7}
+    if(calcEsc === "Energia Interna Temperatura"){const calculoHEHE = 8}
+    if(calcEsc === "Variação de Energia Interna"){const calculoHEHE = 9}
+
     return(
         <div className={ classSC }>
             <div className="cont-calc-in">
@@ -26,8 +47,8 @@ export default function ContCalc() {
                     </div>
                 </div>
                 <div className="calc-botoes">
-                    <button onClick={()=> limpar()}>Limpar</button>
-                    <button onClick={()=> verificarLei(sendbd)}>Calcular</button>
+                    <button onClick={()=> limpar()} className="calc-botoes">Limpar</button>
+                    <button onClick={()=> verificarLei(sendbd)} className="calc-botoes">Calcular</button>
                 </div>
             </div>
         </div>
